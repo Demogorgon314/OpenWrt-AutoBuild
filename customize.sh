61995d980a7d88ab15dbed7b7ff5dff5b711f00e
Demogorgon314 <kwang@streamnative.io>
2022-02-20 23:08:38 +0800

    Change OpenWrt linux kernel to 5.15

--- customize.sh
+++ customize.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#1. Modify default IP
+# 1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
+# 2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+# 3. Change linux kernel to 5.15
+sed -i 's/5.10/5.15/g' openwrt/target/linux/x86/Makefile
