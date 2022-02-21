1b315c3463002403dd1c634ad65cf600159229ba
Demogorgon314 <kwang@streamnative.io>
2022-02-21 09:58:14 +0800

    Update pkg

--- customize.sh
+++ customize.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# 1. Modify default IP
+# Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# 2. Clear the login password
+# Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-# 3. Change linux kernel to 5.15
+# Change linux kernel to 5.15
 sed -i 's/5.10/5.15/g' openwrt/target/linux/x86/Makefile
+
+# Change distrib description
+sed -i "s/OpenWrt /OpenWrt Git Revision $(git rev-parse --short HEAD) Built by Demogorgon314 on $(date +%Y.%m.%d)/g" package/lean/default-settings/files/zzz-default-settings
