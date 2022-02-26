fa4af08b817ac690223f8d11c0c5e974f898d1d6
Demogorgon314 <kwang@streamnative.io>
2022-02-26 18:12:31 +0800

    Update release

--- customize.sh
+++ customize.sh
@@ -5,8 +5,5 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 # Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-# Change linux kernel to 5.15
-sed -i 's/5.10/5.15/g' openwrt/target/linux/x86/Makefile
-
 # Change distrib description
-sed -i "s/OpenWrt /Built by Demogorgon314 on $(date +%Y.%m.%d) /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /OpenWrt Built by Demogorgon314 on $(date +%Y.%m.%d) /g" openwrt/package/lean/default-settings/files/zzz-default-settings
