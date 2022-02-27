11a9edf384c0c6e8b63193d8e7fb74988b910c37
Demogorgon314 <kwang@streamnative.io>
2022-02-27 14:45:45 +0800

    Change default theme to argon

--- customize.sh
+++ customize.sh
@@ -5,5 +5,5 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 # Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-# Change distrib description
+# Change distribute description
 sed -i "s/OpenWrt /OpenWrt Built by Demogorgon314 on $(date +%Y.%m.%d) /g" openwrt/package/lean/default-settings/files/zzz-default-settings
